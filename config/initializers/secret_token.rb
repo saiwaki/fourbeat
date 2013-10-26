b3657d8e4f35cfd43db8e3c713f414195ad09e05
Ken Ohata <ohata@daraf.co.jp>
2013-10-27 00:45:03 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fourbeat::Application.config.secret_key_base = '11a37a9efcdffe8425e0054a91fe132818038dbd90a24aa3c744a417a1df2bebdef3c06433d13fdaef8d9b46e8df5b7981d15add312368d25bf5ea3076ae68dd'
